c949d9ace910dd52b8dd3716e6f3457fbc1291b5
danichol <daneelnichol@outlook.com>
2021-04-02 08:02:35 -0500

    Changes

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired,Email
 class ContactForm(FlaskForm):
     name = TextField('name', validators=[DataRequired()]))
     email = TextField('email', validators=[DataRequired(),Email()])
-    subject = TextField('subject', validators[DataRequired()])
-    body = TextAreaField('textarea',validators[DataRequired()])
+    subject = TextField('subject', validators=[DataRequired()])
+    body = TextAreaField('textarea',validators=[DataRequired()])
     
     
